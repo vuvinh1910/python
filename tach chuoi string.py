993aa0548febef6c4ffb5c0f4546077a9edaa019
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-16 22:00:35 +0700

    Update tach chuoi string.py

--- tach chuoi string.py
+++ tach chuoi string.py
@@ -1,5 +1,5 @@
 # tach chuoi voi dieu kien de tach bang ham split(ky tu tach chuoi)
 s="ha ha ha"
-s.split(" ") # => ha,ha,ha
+s = s.split(" ") # => ha,ha,ha
 s="con1cu1bu"
-s.split("1") # => con,cu,bu
+s = s.split("1") # => con,cu,bu
