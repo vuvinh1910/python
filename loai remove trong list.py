1a3a8ac107a87717bd3b2d8b1084262c88813cd8
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-16 21:18:20 +0700

    Create loai remove trong list.py

---
+++ loai remove trong list.py
@@ -0,0 +1,11 @@
+so=[1,1,2,3]
+so.remove(1) # => 1 2 3
+
+chu["con","con","cu"]
+chu.remove("con") # => con cu
+
+list.remove(i) loai 1 phan tu i duoc tim thay dau tien trong ds list
+
+# loai tat ca phan tu target trong ds list
+while target in list:
+  list.remove(target)
