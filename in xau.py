1950a4a2da38d361425a702ea27b71c0b81c0351
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-16 20:50:00 +0700

    Update in xau.py

--- in xau.py
+++ in xau.py
@@ -5,6 +5,9 @@ vidu xuống dòng:
 print("hello")
 print("4")
 
+# dau phay ngan cach trong in tu dong thanh dau cach
+print("hello",12) # hello 12
+
 vidu in xâu + số:
 c1 : print("1+2 là bằng ",1+2)
 c2 : print("1+2 là bằng " + str(1+2))
