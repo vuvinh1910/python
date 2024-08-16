4985266990153817ea14e34d5a92a20ae666a101
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-16 18:12:51 +0700

    Update if else.py

--- if else.py
+++ if else.py
@@ -1,3 +1,17 @@
+temperature = int(input())
+if temperature >= 100:
+    print("Stay at home and enjoy a good movie")
+elif temperature >= 92:
+    print("Stay at home")
+elif temperature == 75:
+    print("Go outside and enjoy the weather")
+elif temperature < 0:
+    print("It's cool outside")
+else:
+    print("Let's go to school")
+# elif chinh la else ket hop voi if
+
+
 n = int(input())
 if n % 2 == 0:
     # Nếu n là số chẵn thì hiển thị ra màn hình n is an even number
@@ -5,6 +19,7 @@ if n % 2 == 0:
 else:
     # Ngược lại, nếu n không là số chẵn thì hiển thị ra màn hình n is an odd number
     print("n is an odd number")
+
 
 # if else lồng nhau
 a, b = 10, 20
@@ -15,6 +30,7 @@ if a != b:
 		print("b is greater than a")
 else:
 	print("Both a and b are equal")
+
 
 # ham con ket hop tra ve 
 def condition1(age):
