a3eb6f1c00c8e7b073f39022399ce96ade3daa1d
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-16 20:54:59 +0700

    Update lam tron so sau dau phay.py

--- lam tron so sau dau phay.py
+++ lam tron so sau dau phay.py
@@ -1,2 +1,9 @@
 round(number, ndigits)
 #Trong đó number là số cần làm tròn, ndigits là số chữ số sau dấu phẩy cần làm tròn
+
+vd:
+n = int(input())
+count = 0
+for i in range(1,n+1):
+    count += i/(i+1)
+print(round(count,2))
