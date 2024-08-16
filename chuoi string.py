cc70705774cce94876dbd3c91ee205771faf289c
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-16 22:02:47 +0700

    Update chuoi string.py

--- chuoi string.py
+++ chuoi string.py
@@ -1,12 +1,17 @@
 # do dai chuoi
 s="concubu"
 len(s)
+
+# lưu ý là các chuỗi trong Python là bất biến (immutable), 
+# có nghĩa là chúng không thể thay đổi sau khi được tạo
+# muon thay doi thi lam nhu sau: s = s.lower() , neu chi ghi s.lower() thi s khong thay doi
+
 # chuyen chuoi thanh in hoa
-s.upper()
+s=s.upper()
 # chuyen chuoi thanh in thuong
-s.lower()
- kiem tra chuoi chua toan ky tu chu hay khong
-s.isalpha() # true hoac false
- kiem tra chuoi chua toan so hay khong
-s.isnumeric() # true hoac false
+s=s.lower()
+# kiem tra chuoi chua toan ky tu chu hay khong
+ktra = s.isalpha() # true hoac false
+# kiem tra chuoi chua toan so hay khong
+ktra = s.isnumeric() # true hoac false
 
