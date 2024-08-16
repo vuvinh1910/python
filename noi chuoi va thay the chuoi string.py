33f886ab3f60b327ba2a88c683632b1dd1ee9ad3
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-16 22:00:06 +0700

    Create noi chuoi va thay the chuoi string.py

---
+++ noi chuoi va thay the chuoi string.py
@@ -0,0 +1,9 @@
+# noi chuoi bang ham join(ky tu noi)
+vd:
+list=["1","2","3"]
+list = list.join("-") # => "1-2-3"
+
+thay the chuoi bang ham replace(ky tu can thay the, ky tu thay the)
+vd:
+list = "concu"
+list = list.replace("c","v") # => vonvu
