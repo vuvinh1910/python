2a9d809df067a42e29c31a8422dab3114c1c0f4a
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-16 22:13:31 +0700

    Create swap.py

---
+++ swap.py
@@ -0,0 +1,14 @@
+# trong bien
+a = 5
+b = 10
+# Hoán đổi giá trị của a và b
+a, b = b, a
+print("a =", a)  # Kết quả: a = 10
+print("b =", b)  # Kết quả: b = 5
+
+# trong danh sach
+lst = [1, 2, 3, 4, 5]
+# Hoán đổi phần tử đầu tiên và phần tử cuối cùng
+lst[0], lst[-1] = lst[-1], lst[0]
+print(lst)  # Kết quả: [5, 2, 3, 4, 1]
+
