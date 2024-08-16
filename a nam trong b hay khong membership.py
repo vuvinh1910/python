3cd5f95caf223e84255ce829f1700f67a99020bc
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-16 17:48:15 +0700

    Create a nam trong b hay khong membership.py

---
+++ a nam trong b hay khong membership.py
@@ -0,0 +1,6 @@
+# in và not in là hai toán tử membership với cách sử dụng rất đơn giản.
+# Ví dụ bạn có thể kiểm tra xem một xâu có nằm trong một xâu khác không
+
+print("Code" in "Codelearn") # return true
+print("Py" in "Python") # return true
+print("x" in "python") # return false
