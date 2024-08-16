c3efb152f124b6368bf640e2c636c00d98324ed9
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-16 20:57:04 +0700

    Create danh sach list.py

---
+++ danh sach list.py
@@ -0,0 +1,12 @@
+# Tạo ra list để lưu trữ các số nguyên
+list1 = [1, 2, 3]
+# Tạo ra list để lưu trữ các xâu ký tự
+list2 = ["Viet", "Tuan", "Duong"]
+# Bạn cũng có thể tạo ra một list lưu trữ các kiểu dữ liệu khác nhau
+list3 = [7, 3.5, "Codelearn"]
+
+# Để truy xuất tới các phần tử trong list bạn dùng toán tử []. Ví dụ:
+names = ["Viet", "Dung", "Huong"]
+print(names[0])
+print(names[1])
+print(names[2])
