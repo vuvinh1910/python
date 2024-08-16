a21dbc772b8570899dbb05d0b5868895200f4c3b
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-16 22:36:55 +0700

    Update tach chuoi string.py

--- tach chuoi string.py
+++ tach chuoi string.py
@@ -4,3 +4,12 @@ s="ha ha ha"
 s = s.split(" ") # => ha,ha,ha
 s="con1cu1bu"
 s = s.split("1") # => con,cu,bu
+
+s = input("Nhập chuỗi: ")
+s = s.split(" ")  # Tách chuỗi thành danh sách các từ
+ds = []
+for i in range(len(s)-1, -1, -1):
+    ds.append(s[i])
+ds = " ".join(ds)  # Nối danh sách ds thành chuỗi, ngăn cách bằng dấu cách
+print(ds)
+
