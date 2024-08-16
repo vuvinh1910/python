b472d1704484bbd5a0b958201a653c83c498ccdf
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-16 21:27:41 +0700

    Update and rename loai remove trong list.py to xoa phan tu trong list.py

--- xoa phan tu trong list.py
+++ xoa phan tu trong list.py
@@ -1,3 +1,4 @@
+# remove trong list
 so=[1,1,2,3]
 so.remove(1) # => 1 2 3
 
@@ -9,3 +10,9 @@ list.remove(i) loai 1 phan tu i duoc tim thay dau tien trong ds list
 # loai tat ca phan tu target trong ds list
 while target in list:
   list.remove(target)
+
+# pop() trong list
+lst = ['A', 'B', 'C']
+# Xóa phần tử thứ 2 khỏi list
+lst.pop(1)
+print(lst) # ['A', 'C']
