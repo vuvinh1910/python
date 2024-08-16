6f90df690296dde5d7d7f95cf8739000e4ac4575
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-16 20:38:17 +0700

    Create vong lap.py

---
+++ vong lap.py
@@ -0,0 +1,23 @@
+# lap qua danh sach
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+  
+# lap qua chuoi
+for char in "hello":
+    print(char)
+
+# lap voi range()
+# range(start, stop, step)
+# start: Điểm bắt đầu của dãy (bao gồm).
+# stop: Điểm kết thúc của dãy (không bao gồm).
+# step: Bước nhảy giữa các giá trị.
+for i in range(0, 8, 2):
+    print(i)
+
+# vong lap while
+count = 0
+while count < 5:
+    print(count)
+    count += 1  # Cập nhật điều kiện
+
