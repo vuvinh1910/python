67a1991f1733c87a9f13be0ef369c36a2d7b6246
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-16 17:21:29 +0700

    Update nhap tu ban phim.py

--- nhap tu ban phim.py
+++ nhap tu ban phim.py
@@ -1,2 +1,8 @@
 name = input("Nhập tên của bạn: ")
 print("Xin chào", name)
+
+#nhap so nguyen
+# input() luon tra ve str nen khi nhap so nguyen can dung lenh int(input())
+vd: 
+age = int(input())
+print(2+age)
