c9f0d1e26bc3d5e5b9418c045e02309a3dd3db78
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-16 22:29:18 +0700

    Update tach chuoi string.py

--- tach chuoi string.py
+++ tach chuoi string.py
@@ -1,4 +1,5 @@
 # tach chuoi voi dieu kien de tach bang ham split(ky tu tach chuoi)
+# co the thay s tu string thay doi thanh dang danh sach list
 s="ha ha ha"
 s = s.split(" ") # => ha,ha,ha
 s="con1cu1bu"
