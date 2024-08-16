36cb58cc39be9d13511bf47f547bb75d4032422c
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-16 21:51:37 +0700

    Create chuoi string.py

---
+++ chuoi string.py
@@ -0,0 +1,12 @@
+# do dai chuoi
+s="concubu"
+len(s)
+# chuyen chuoi thanh in hoa
+s.upper()
+# chuyen chuoi thanh in thuong
+s.lower()
+ kiem tra chuoi chua toan ky tu chu hay khong
+s.isalpha() # true hoac false
+ kiem tra chuoi chua toan so hay khong
+s.isnumeric() # true hoac false
+
