9a6e1144292b4b7184bb2e860c00e9ab1d1c7b46
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-17 22:04:54 +0700

    Create ep kieu.py

---
+++ ep kieu.py
@@ -0,0 +1,14 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    else:
+        for i in range(2,int(n**0.5)+1,1): # ep float thanh int
+            if n%i == 0:
+                return False
+    return True
+
+
+n = int(input())
+print(is_prime(n))
+
+# ktra so nguyen to
