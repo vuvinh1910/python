4f8f2c064f80f7b8ed2090c59b7aacfe9cbc5a60
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-20 22:54:37 +0700

    Update chuyen sang co so 2.py

--- chuyen sang co so 2.py
+++ chuyen sang co so 2.py
@@ -1,3 +1,8 @@
 binary_str = "1010"  # Nhị phân 1010
-decimal_value = int(binary_str, 2)
+decimal_value = int(binary_str, 2) # vi la nhi phan nen o day la 2
 print(decimal_value)  # Output: 10
+
+hex_str = "FF"  # Thập lục phân FF
+decimal_value = int(hex_str, 16) # thap luc phan la he 16
+print(decimal_value)  # Output: 255
+
