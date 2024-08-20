df044f3decfc7f4f6edbffc2970e0737396c3f97
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-20 22:58:13 +0700

    Create viet hoa viet thuong.py

---
+++ viet hoa viet thuong.py
@@ -0,0 +1,11 @@
+text = "hello world"
+upper_text = text.upper()
+print(upper_text)  # Output: 'HELLO WORLD'
+
+text = "HELLO WORLD"
+lower_text = text.lower()
+print(lower_text)  # Output: 'hello world'
+
+text = "Hello World"
+swapcase_text = text.swapcase()
+print(swapcase_text)  # Output: 'hELLO wORLD'
