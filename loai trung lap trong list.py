af9bbf06257ed47f4d971e51783e728fbaa23a7b
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-09-13 08:58:38 +0700

    Update loai trung lap trong list.py

--- loai trung lap trong list.py
+++ loai trung lap trong list.py
@@ -1,3 +1,3 @@
 my_list = [1, 2, 2, 3, 4, 4, 5, 5, 5]
-my_list = list(set(my_list))
+my_list = list(set(my_list)) # đưa về set loại trùng lặp rồi chuyển về list
 print(my_list)  # Output: [1, 2, 3, 4, 5]
