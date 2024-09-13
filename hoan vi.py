02142364161de782d44dc1373464ed2164e475b8
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-09-13 10:30:25 +0700

    Update hoan vi.py

--- hoan vi.py
+++ hoan vi.py
@@ -8,4 +8,11 @@ perm_list = permutations(s)
 
 # In từng hoán vị
 for perm in perm_list:
-    print(''.join(perm))
+    print(''.join(perm)) # perm la mot set trong perm_list
+
+# Kiểm tra và in hoán vị thứ 3 nếu có
+if len(perm_list) >= 3:
+    third_permutation = ''.join(perm_list[2])
+    print(third_permutation)
+else:
+    print("Không đủ hoán vị")
