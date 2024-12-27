d6dce93227fe689decdb43772605182647b095fa
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-12-27 22:07:08 +0700

    Update lam tron so sau dau phay.py

--- lam tron so sau dau phay.py
+++ lam tron so sau dau phay.py
@@ -1,14 +1,17 @@
+import math
 # round(number, ndigits)
 #Trong đó number là số cần làm tròn, ndigits là số chữ số sau dấu phẩy cần làm tròn
-
 # vd:
-n = int(input())
-count = 0
-for i in range(1,n+1):
-    count += i/(i+1)
-print(round(count,2))
+n = 12.88888
+print(int(round(n,0))) # ket qua = 13
+print(round(n,3)) # ket qua = 12.889
 
-
+# lam tron bang f-print
 tong = 3.17777777
-print(f"{tong:.6f}")
-# in ra 6 số sau dấu phẩy co làm tròn
+print(f"{tong:.6f}")  # in ra 6 số sau dấu phẩy co làm tròn
+
+# lam tron len den phan nguyen
+print(math.ceil(tong)) # ket qua = 4
+
+# lam tron xuong den phan nguyen
+print(math.floor(n)) # ket qua = 12
