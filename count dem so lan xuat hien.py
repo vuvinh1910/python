e21606797745c0cbf058a3799e1b265cda7a1ac7
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-12-27 21:55:02 +0700

    Create count dem so lan xuat hien.py

---
+++ count dem so lan xuat hien.py
@@ -0,0 +1,8 @@
+#trong list
+my_list = [1, 2, 3, 2, 4, 2]
+print(my_list.count(2))  # Output: 3
+
+# trong string
+# string.count(str, start, end)
+my_string = "hello world, hello python"
+print(my_string.count("o", 5, 15))  # Output: 2
