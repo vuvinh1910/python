073dbaf06d61fc0f18547886a5f0c19d20203a7f
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-12-27 21:56:49 +0700

    Update noi chuoi va thay the chuoi string.py

--- noi chuoi va thay the chuoi string.py
+++ noi chuoi va thay the chuoi string.py
@@ -1,9 +1,12 @@
 # noi chuoi bang ham join(ky tu noi)
-vd:
+# vd:
 list=["1","2","3"]
 list = "-".join(list) # => "1-2-3"
 
-thay the chuoi bang ham replace(ky tu can thay the, ky tu thay the)
+# thay the chuoi bang ham replace(ky tu can thay the, ky tu thay the, so lan thay the)
+# neu ko nhap so lan thay the thi mac dinh no se thay the tat ca chuoi con
+# string.replace(old, new, count)
+
 vd:
 list = "concu"
 list = list.replace("c","v") # => vonvu
