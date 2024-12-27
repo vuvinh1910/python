371bae7da01a589306754db1f168715755cae270
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-12-27 15:26:50 +0700

    Update phan_tich_SNT.py

--- thuat toan/phan_tich_SNT.py
+++ thuat toan/phan_tich_SNT.py
@@ -1,12 +1,12 @@
 import math
 
 def SNT(n):
-  for i in range(2,math.isqrf(n)+1):
+  for i in range(2,math.isqrt(n)+1):
     while(n%i==0):
       print(i,end="*")
       n = n//i
   if(n>1):
     print(n,end='')
     
-a = 19843
+a = 60
 SNT(a)
