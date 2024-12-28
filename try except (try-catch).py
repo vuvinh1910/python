263586497b2563878c30aa40993fb054c1439c9d
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-12-28 21:43:04 +0700

    Create try except (try-catch).py

---
+++ try except (try-catch).py
@@ -0,0 +1,8 @@
+try:
+    # Code có thể gây ra ngoại lệ
+except Exception:
+    # Xử lý khi xảy ra ngoại lệ
+else:
+    # Thực thi khi không có ngoại lệ xảy ra
+finally:
+    # Luôn được thực thi, bất kể có ngoại lệ hay không
