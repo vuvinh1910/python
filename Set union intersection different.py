3d0aa390f22542d32549f9b1d3612990df60fe22
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-12-28 15:59:32 +0700

    Create Set union intersection different.py

---
+++ Set union intersection different.py
@@ -0,0 +1,14 @@
+set1 = {1, 2, 3}
+set2 = {2, 3, 4}
+
+# giao
+result = set1 & set2
+print(result)  # Output: {2, 3}
+
+# hop
+result = set1 | set2
+print(result)  # Output: {1, 2, 3, 4}
+
+# hieu:
+result = set1 - set2
+print(result)  # Output: {1}
