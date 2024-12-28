3c8f774c913bb22713c2ce3efdf250b3635e8db9
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-12-28 15:55:17 +0700

    Create set.py

---
+++ set.py
@@ -0,0 +1,12 @@
+# cac phan tu khong giong nhau
+# khong the truy cap cac phan tu thong qua vi tri i set[i]
+# chi co them hoac xoa, khong the sua
+# set luu duoc tuple nhung khong luu duoc list vi list co the thay doi duoc
+
+set = {1,2,3,4,5}
+set.add(6)
+set.remove(1) # xoa so 1 trong set
+
+# dung lenh in de tim kiem ptu trong set: (vi set giup tim kiem cuc nhanh)
+i = 4
+print(i in set)
